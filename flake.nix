1f518d91664ac338dbc2b864be4d0baf2bc0e1d2
MBanucu <michael.banucu@googlemail.com>
2026-05-26 21:11:54 +0200

    build: add Nix flake packaging support
    Introduce Nix flake configuration and package expression to build
    exfat-raw with Nix, enabling reproducible builds, development
    shells, and integration via overlays.

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "exfat-raw: Raw block-level read/write of exFAT filesystem timestamps";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ self.overlays.default ];
+        };
+      in
+      {
+        packages.default = pkgs.exfat-raw;
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [ python3 ];
+        };
+      }
+    )
+    // {
+      overlays.default = final: prev: {
+        exfat-raw = final.python3.pkgs.callPackage ./nix/exfat-raw { };
+      };
+    };
+}
