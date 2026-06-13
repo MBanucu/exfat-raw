a9c64832521ada0f19765830f6e60134a1934b82
MBanucu <michael.banucu@googlemail.com>
2026-06-13 16:07:13 +0200

    ci: install project dependencies before running tests
    Add pip install step to test workflow and add rawblock-io as a Nix
    build input so the extracted dependency is available in CI.

--- nix/exfat-raw/default.nix
+++ nix/exfat-raw/default.nix
@@ -2,17 +2,32 @@
   lib
 , buildPythonPackage
 , setuptools
+, fetchPypi
 , src
 }:
 
+let
+  rawblock-io = buildPythonPackage rec {
+    pname = "rawblock-io";
+    version = "0.1.0";
+    src = fetchPypi {
+      pname = "rawblock-io";
+      version = "0.1.0";
+      sha256 = "a68b5a019d4d29d92e39504d6f057660d2d62307d7a7d429d03647ce42b1c7e1";
+    };
+    pyproject = true;
+    nativeBuildInputs = [ setuptools ];
+  };
+in
 buildPythonPackage rec {
   pname = "exfat-raw";
-  version = "0.1.2";
+  version = "0.2.1";
   pyproject = true;
 
   inherit src;
 
   nativeBuildInputs = [ setuptools ];
+  propagatedBuildInputs = [ rawblock-io ];
   doCheck = true;
   pythonImportsCheck = [ "exfat_raw" ];
 
